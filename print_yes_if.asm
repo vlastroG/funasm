574b2a0d0c873173a14e0e71b13ae4cbdef34ee6
vlastroG <vlastroganov@gmail.com>
2023-09-27 22:09:15 +0300

    Add comments

--- print_yes_if.asm
+++ print_yes_if.asm
@@ -3,9 +3,9 @@
 global	_start
 
 section	.text
-_start:	GETCHAR
-	cmp	al, 65
-	jne 	finish
-	PRINT "YES"
-	PUTCHAR 10
+_start:	GETCHAR		;read char and write it to EAX. It will be fully in AL
+	cmp	al, 65	;compare char ascii-code with 'A' ascii-code
+	jne 	finish	;if not equal finish go to finish
+	PRINT "YES"	;print "YES"
+	PUTCHAR 10	;print new line char
 finish:	FINISH
