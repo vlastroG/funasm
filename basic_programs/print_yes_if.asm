548a0e99af2b413448e36982342808c5b137d36b
vlastroG <vlastroganov@gmail.com>
2023-09-27 22:35:18 +0300

    Code style

--- basic_programs/print_yes_if.asm
+++ basic_programs/print_yes_if.asm
@@ -1,11 +1,12 @@
-;This program read one char and prints 'YES', if the char is 'A'
+;; This program read one char and prints 'YES', if the char is 'A'
+
 %include "../stud_io.inc"
 global	_start
 
 section	.text
-_start:	GETCHAR		;read char and write it to EAX. It will be fully in AL
-	cmp	al, 65	;compare char ascii-code with 'A' ascii-code
-	jne 	finish	;if not equal finish go to finish
-	PRINT "YES"	;print "YES"
-	PUTCHAR 10	;print new line char
+_start:	GETCHAR		; read char and write it to EAX. It will be fully in AL
+	cmp	al, 65	; compare char ascii-code with 'A' ascii-code
+	jne 	finish	; if not equal finish go to finish
+	PRINT "YES"	; print "YES"
+	PUTCHAR 10	; print new line char
 finish:	FINISH
