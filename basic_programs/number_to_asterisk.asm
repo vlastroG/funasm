95a2bcecd77f9a1014a56a19fa480ed7cf432d55
vlastroG <vlastroganov@gmail.com>
2023-09-28 19:35:09 +0300

    Add number to asterisk program

---
+++ basic_programs/number_to_asterisk.asm
@@ -0,0 +1,17 @@
+;; This program reads one char, and if it is a number, prints asterisk count
+
+%include "../stud_io.inc"
+global	_start
+
+section	.text
+_start:	GETCHAR		; read char
+	cmp	al, 49	; compare char and "1"
+	jl	finish	; if less then finish
+	cmp	al, 57	; com[are char and "9"
+	jg	finish	; if greater then finish
+	sub	al, 48	; convert char code to number
+	mov	cl, al	; set count for cycle
+lp:	PUTCHAR "*"	; print "*"
+	loop	lp
+	PUTCHAR	10	; print new line char
+finish:	FINISH
